742348cb043057454aa15680f38b77123ee2d475
JoaoRB163 <joaobataglion087@gmail.com>
2026-08-10 22:18:49 -0300

    corzinha

--- aula02/app/src/main/java/com/example/aula2/MainActivity.java
+++ aula02/app/src/main/java/com/example/aula2/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.aula2;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +11,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
 
+public class MainActivity extends AppCompatActivity {
+    int i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button butao = findViewById(R.id.button);
+        TextView t = findViewById(R.id.textView);
+
+        Random random = new Random();
+
+
+
+
+
+        butao.setOnClickListener(view -> {
+            int r = random.nextInt(256);
+            int g = random.nextInt(256);
+            int b = random.nextInt(256);
+            findViewById(R.id.main).setBackgroundColor(Color.rgb(r, g, b));
+            t.setText(i);
+            i++;
+        });
+
     }
 }
