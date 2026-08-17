f3fd105a7f8482dfdb7ff55d0ad3d0d2d2cd8c80
joaoRB163 <joaobataglion087@gmail.com>
2026-08-17 19:56:04 -0300

    correçõeszinhas

--- aula03/app/src/main/java/com/example/aula03/MainActivity.java
+++ aula03/app/src/main/java/com/example/aula03/MainActivity.java
@@ -1,9 +1,6 @@
-package com.example.aula2;
+package com.example.aula03;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,10 +8,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.Random;
-
 public class MainActivity extends AppCompatActivity {
-    int i = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,20 +20,5 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        Button butao = findViewById(R.id.button);
-        TextView t = findViewById(R.id.textView);
-
-        Random random = new Random();
-
-        butao.setOnClickListener(view -> {
-            int r = random.nextInt(256);
-            int g = random.nextInt(256);
-            int b = random.nextInt(256);
-            findViewById(R.id.main).setBackgroundColor(Color.rgb(r, g, b));
-            i++;
-            t.setText("Hello World!\n" + "\t\t\t\t" + Integer.toString(i));
-        });
-
     }
 }
